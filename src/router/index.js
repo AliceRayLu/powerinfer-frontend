455a9678d3fdb5ba770c9ab1be4c3963bf8c0663
luboheng <211250054@smail.nju.edu.cn>
2025-03-28 17:05:58 +0800

    add router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+    scrollBehavior () {
+        return {top: 0}
+    }
+})
+
+export default router;
